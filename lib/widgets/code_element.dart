fb46cc5a335bb08bc0f1ead6e17a17d7b2b95b09
peron <peron_ap@icloud.com>
2023-12-11 12:12:14 +0000

    feat: use dark theme for code

--- lib/widgets/code_element.dart
+++ lib/widgets/code_element.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_highlighter/flutter_highlighter.dart';
 import 'package:flutter_highlighter/themes/atom-one-dark.dart';
-import 'package:flutter_highlighter/themes/atom-one-light.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:get/get.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:google_fonts/google_fonts.dart';
 
 class CodeElementBuilder extends MarkdownElementBuilder {
   @override
   Widget? visitElementAfter(md.Element element, TextStyle? preferredStyle) {
-    final brightness = Get.theme.brightness;
-
     var language = '';
 
     if (element.attributes['class'] != null) {
@@ -30,13 +26,10 @@ class CodeElementBuilder extends MarkdownElementBuilder {
         language: language,
 
         // Specify highlight theme
-        // All available themes are listed in `themes` folder
-        theme: brightness == Brightness.light
-            ? atomOneLightTheme
-            : atomOneDarkTheme,
+        theme: atomOneDarkTheme,
 
         // Specify padding
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
 
         // Specify text style
         textStyle: GoogleFonts.robotoMono(),
