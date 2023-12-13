80ce1f05c12a85d9699d9504bc4bd1437adde2cf
peron <peron_ap@icloud.com>
2023-12-13 16:59:01 +0000

    feat: copy code

--- lib/widgets/code_element.dart
+++ lib/widgets/code_element.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_highlighter/flutter_highlighter.dart';
 import 'package:flutter_highlighter/themes/atom-one-dark.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:get/get.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:google_fonts/google_fonts.dart';
 
@@ -24,29 +26,52 @@ class CodeElementBuilder extends MarkdownElementBuilder {
               fontFamily: GoogleFonts.robotoMono().fontFamily));
     }
 
+    void copyContent() async {
+      try {
+        await Clipboard.setData(ClipboardData(text: textContent));
+        Get.snackbar('Copied', 'Code has been copied to clipboard');
+      } catch (e) {
+        Get.snackbar(
+            'Error Occurred', 'Failed to copy message to clipboard: $e');
+      }
+    }
+
     // render code block
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       // https://stackoverflow.com/questions/59592640/how-to-add-code-syntax-highlighter-to-flutter-markdown
-      child: HighlightView(
-        // The original code to be highlighted
-        textContent,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(language.isEmpty ? 'code' : language,
+                  style: GoogleFonts.robotoMono()),
+              IconButton(
+                  onPressed: copyContent, icon: const Icon(Icons.copy_rounded)),
+            ],
+          ),
+          HighlightView(
+            // The original code to be highlighted
+            textContent.trim(),
 
-        // Specify language
-        // It is recommended to give it a value for performance
-        language: language,
+            // Specify language
+            // It is recommended to give it a value for performance
+            language: language,
 
-        // Specify highlight theme
-        theme: atomOneDarkTheme,
+            // Specify highlight theme
+            theme: atomOneDarkTheme,
 
-        // Specify padding
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            // Specify padding
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
 
-        // Specify text style
-        textStyle: GoogleFonts.robotoMono(),
+            // Specify text style
+            textStyle: GoogleFonts.robotoMono(),
 
-        // Specify tab size
-        tabSize: 4,
+            // Specify tab size
+            tabSize: 4,
+          )
+        ],
       ),
     );
   }
