5015ffa99a6282212d1599e8bd47380c0abccc58
peron <peron_ap@icloud.com>
2023-12-14 23:23:13 +0000

    feat: limit max width

--- lib/widgets/message_list.dart
+++ lib/widgets/message_list.dart
@@ -10,46 +10,50 @@ class MessageList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: GetBuilder<Controller>(
-          id: Controller.idMessageList,
-          builder: (controller) {
-            return ListView.builder(
-              controller: controller.scrollController,
-              itemCount: controller.messages.length + 1,
-              itemBuilder: (context, index) {
-                if (index == controller.messages.length) {
-                  return Obx(() => controller.isGenerating.value
-                      // The message being generated
-                      ? MessageTile(
-                          index: index,
-                          isBeingGenerated: true,
-                          message: Message(
-                              role: Message.roleAssistant,
-                              type: controller.generatingType.value,
-                              content: controller.generatingContent.value))
-                      // User input
-                      : MessageTile(
-                          index: index,
-                          isBeingGenerated: true,
-                          message: Message(
-                            role: Message.roleUser,
-                            type: Message.typeTyping,
-                            content: controller.prompt.value,
-                            imageUrls: controller.sydneyService.imageUrl.isEmpty
-                                ? null
-                                : [controller.sydneyService.imageUrl.value],
-                          )));
-                }
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 1024),
+        child: GetBuilder<Controller>(
+            id: Controller.idMessageList,
+            builder: (controller) {
+              return ListView.builder(
+                controller: controller.scrollController,
+                itemCount: controller.messages.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == controller.messages.length) {
+                    return Obx(() => controller.isGenerating.value
+                        // The message being generated
+                        ? MessageTile(
+                            index: index,
+                            isBeingGenerated: true,
+                            message: Message(
+                                role: Message.roleAssistant,
+                                type: controller.generatingType.value,
+                                content: controller.generatingContent.value))
+                        // User input
+                        : MessageTile(
+                            index: index,
+                            isBeingGenerated: true,
+                            message: Message(
+                              role: Message.roleUser,
+                              type: Message.typeTyping,
+                              content: controller.prompt.value,
+                              imageUrls: controller
+                                      .sydneyService.imageUrl.isEmpty
+                                  ? null
+                                  : [controller.sydneyService.imageUrl.value],
+                            )));
+                  }
 
-                // Render normal message
-                final message = controller.messages[index];
-                return MessageTile(
-                  index: index,
-                  message: message,
-                );
-              },
-            );
-          }),
+                  // Render normal message
+                  final message = controller.messages[index];
+                  return MessageTile(
+                    index: index,
+                    message: message,
+                  );
+                },
+              );
+            }),
+      ),
     );
   }
 }
