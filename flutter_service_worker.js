955bcb7a98337d3ea13bfcf3c287fe9a2ea2a9a4
peron <peron_ap@icloud.com>
2023-12-16 10:34:45 +0000

    Built web
    Branch: main
    Commit: 2ff0b112701d97d4f81bd6de007b2c985db7e659 (dirty)
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "6c17129986e73894978d73c0b0c629f5",
-"index.html": "de1701d0ccede5da7f86946f2611eec7",
-"/": "de1701d0ccede5da7f86946f2611eec7",
-"main.dart.js": "179d324abafe17c5a8223aa607881dc1",
+"index.html": "72a78a295559c5a0c48733dd2a622f97",
+"/": "72a78a295559c5a0c48733dd2a622f97",
+"main.dart.js": "afcb88f927c1bf4ec7968eba47d42303",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
