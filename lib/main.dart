e193324fb64910ce6d9a5a9c59907e52a5126264
peron <peron_ap@icloud.com>
2023-12-17 00:35:43 +0000

    feat: different color every time

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -5,13 +7,16 @@ import 'package:sydney_webui/pages/chat_page.dart';
 
 void main() async {
   await GetStorage.init();
+
+  final colorIndex = Random().nextInt(Colors.primaries.length);
+  final color = Colors.primaries[colorIndex];
+
   runApp(
     GetMaterialApp(
         title: "Sydney Chat",
         home: ChatPage(),
-        theme: ThemeData(
-            colorSchemeSeed: Colors.teal, brightness: Brightness.light),
-        darkTheme: ThemeData(
-            colorSchemeSeed: Colors.teal, brightness: Brightness.dark)),
+        theme: ThemeData(colorSchemeSeed: color, brightness: Brightness.light),
+        darkTheme:
+            ThemeData(colorSchemeSeed: color, brightness: Brightness.dark)),
   );
 }
