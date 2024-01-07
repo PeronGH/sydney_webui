fe0d3889b9a1b4f59090dad916b56a21d5eef30c
peron <peron_ap@icloud.com>
2024-01-07 18:12:52 +0000

    feat: avoid duplicated uploading

--- lib/services/sharegpt_service.dart
+++ lib/services/sharegpt_service.dart
@@ -2,32 +2,41 @@ import 'package:get/get.dart';
 import 'package:sydney_webui/models/message.dart';
 
 class ShareGptService extends GetConnect {
+  var isUploading = false;
+
   Future<String> uploadConversation(List<Message> messages) async {
-    final items = messages
-        .map((msg) => switch (msg) {
-              Message(
-                role: Message.roleUser,
-                type: Message.typeMessage,
-                content: _
-              ) =>
-                {"from": "user", "value": msg.content},
-              Message(
-                role: Message.roleAssistant,
-                type: Message.typeMessage,
-                content: _
-              ) =>
-                {"from": "gpt", "value": msg.content},
-              _ => null
-            })
-        .where((e) => e != null)
-        .cast<Map<String, String>>()
-        .toList();
+    if (isUploading) throw Exception("Already uploading");
+    isUploading = true;
 
-    if (items.isEmpty) throw Exception("No message to upload");
+    try {
+      final items = messages
+          .map((msg) => switch (msg) {
+                Message(
+                  role: Message.roleUser,
+                  type: Message.typeMessage,
+                  content: _
+                ) =>
+                  {"from": "user", "value": msg.content},
+                Message(
+                  role: Message.roleAssistant,
+                  type: Message.typeMessage,
+                  content: _
+                ) =>
+                  {"from": "gpt", "value": msg.content},
+                _ => null
+              })
+          .where((e) => e != null)
+          .cast<Map<String, String>>()
+          .toList();
 
-    final response =
-        await post("https://sharegpt.com/api/conversations", {"items": items});
+      if (items.isEmpty) throw Exception("No message to upload");
 
-    return response.body["id"];
+      final response = await post(
+          "https://sharegpt.com/api/conversations", {"items": items});
+
+      return response.body["id"];
+    } finally {
+      isUploading = false;
+    }
   }
 }
